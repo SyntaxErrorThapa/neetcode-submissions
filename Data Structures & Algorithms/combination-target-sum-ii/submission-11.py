141e309f5ba0369b23010f4a74d4d0269f03aebf
Pratik Thapa <125596457+SyntaxErrorThapa@users.noreply.github.com>
2026-08-19 16:59:47 -0400

    Add: combination-target-sum-ii - submission-11

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        def helper(i, cur, total):
+            # Base Case 
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            for j in range(i, len(candidates)):
+                if j > i and candidates[j] == candidates[j-1]:
+                    continue
+                else:
+                    cur.append(candidates[j])
+                    helper(j + 1, cur, total + candidates[j])
+                    cur.pop()
+
+        helper(0, [], 0)
+        return res
